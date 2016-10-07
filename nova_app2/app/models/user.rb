51d0af737a74ca14143cd95ee50cad0682a114ec
Jan Hlisnikovsky <jan.hlisnikovsky@topmonks.com>
2016-10-07 13:47:55 +0200

    initial commit

---
+++ nova_app2/app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  rolify
+def self.from_omniauth(access_token)
+              data = access_token.info
+              user = User.where(:email => data['email']).first
+
+              # Uncomment the section below if you want users to be created if they don't exist
+              unless user
+                  user = User.create(email: data['email'],
+                     password: Devise.friendly_token[0,20]
+                  )
+              end
+              user
+          end
+devise :omniauthable, :omniauth_providers => [:google_oauth2]
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
